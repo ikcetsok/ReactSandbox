fe2c6511a4370e997e681ba95f7de61b03e464c7
ikcetsok <panicz.kostek@gmail.com>
2022-08-22 07:53:48 +0200

    z

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,10 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button class="btn btn-danger">Click here</button>;
         <a
           className="App-link"
           href="https://reactjs.org"
